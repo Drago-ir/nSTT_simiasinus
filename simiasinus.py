e7ebbf5434f948e06f778327c34b9aeaabf37d8d
thomas.dutrannois@outlook.com <thomas.dutrannois@outlook.com>
2024-02-20 15:03:12 +0100

    added whishper

--- simiasinus.py
+++ simiasinus.py
@@ -14,14 +14,13 @@ def filter(path_to_noisy_speech_file) :
 
     return path_to_denoised_speech_file
 
-def STT() :
+def STT(path_to_noisy_speech_file) :
 
-    
     model_size = "large-v3"
     # or run on CPU with INT8
     model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-    segments, info = model.transcribe("audio.mp3", beam_size=5)
+    segments, info = model.transcribe(path_to_noisy_speech_file, beam_size=5)
 
     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
@@ -35,9 +34,14 @@ def nSTT(path_to_noisy_speech_file) :
     #filter out noise from the signal
     path_to_denoised_speech_file = filter(path_to_noisy_speech_file_in_wav_format)
 
-    #provide clean speech to wishper
+    #provide clean speech to wishper (running on noisy for the moment)
+    STT(path_to_noisy_speech_file)
 
 
+def main() :
+
+    nSTT("/home/thomas/projects/tide_hackathon/data/deep_audi_denoiser/Noisy Input.mp3")
 
 
-
+if __name__ == "__main__":
+    main()
